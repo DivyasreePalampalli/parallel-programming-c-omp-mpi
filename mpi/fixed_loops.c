fc850e1bdb57ff4c8a6fb6f5f0d625956ea9a2b1
Bhoomika-B-Reddy <bhoomikareddys2004@gmail.com>
2026-02-19 11:57:18 +0530

    Fix loop iteration count for MPI processes

--- mpi/fixed_loops.c
+++ mpi/fixed_loops.c
@@ -11,27 +11,30 @@ int main(int argc, char **argv)
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
 
-    // we want to perform 1002 iterations in total. Work out the 
+    // we want to perform 1000 iterations in total. Work out the 
     // number of iterations to perform per process...
-    count = 1002 / nprocs;
+    count = 1000 / (nprocs-1);
 
     // we use the rank of this process to work out which
     // iterations to perform.
-    start = rank * count;
+    if(rank!=0){
+    start = (rank-1) * count;
     stop = start + count;
+    }
 
     // now perform the loop
     nloops = 0;
-
+    if(rank!=0){
     for (i=start; i<stop; ++i)
     {
         ++nloops;
+    }
     }
 
     printf("Process %d performed %d iterations of the loop.\n",
            rank, nloops);
 
-//p0,1,2,3 ==> nloops 250
+//p1,2,3 ==> nloops 333
     if (rank != 0)
     {
         // if we are not the master process (rank 0) then we need to
@@ -55,7 +58,7 @@ int main(int argc, char **argv)
         // ok - are there any more loops to run?
         nloops = 0;
 
-        for (i=total_nloops; i<1002; ++i)
+        for (i=total_nloops; i<1000; ++i)
         {
             ++nloops;
         }
